3555e5de3ea40ed2e1989c16adc0ea5c9644b00b
Harold Williams <haroldwgc@gmail.com>
2022-08-29 09:50:53 -0400

    gh-pages added

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -env
+
+npm run build
+
+cd dist
+
+git init
+
+git checkout -b main
+
+git add -add
+
+git commit -m 'deploy'
+
+git push -f git@github.com:haroldwgc/presupuesto.git main:gh-pages
+
